aaeaada76b73c4de38fecd25def000eafb97d7ed
Alex26 <alex26@Ubuntu.myguest.virtualbox.org>
2023-04-18 01:20:53 +0300

    add deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello my frinds!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
